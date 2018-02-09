2b6dae1fe09412a5bc1e26f396bebdf8cf1a3c5e
Derek Olsen <someword@gmail.com>
2018-02-09 09:34:02 -0800

    cp

--- pkg/k8snode/aws.go
+++ pkg/k8snode/aws.go
@@ -1,8 +1,8 @@
 package k8snode
 
 import (
-	"github.com/aws/aws-sdk-go/AWS/session"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/golang/glog"
 )
